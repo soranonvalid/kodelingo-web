60fae7850abaac03b6b18240fe730fc0d3a01be5
DiqySH <margolang55@gmail.com>
2025-11-09 22:04:41 +0700

    fix bug chat

--- src/components/ChatContainer.tsx
+++ src/components/ChatContainer.tsx
@@ -1,80 +1,65 @@
-import type { ChatMessage, FirebaseUser } from "@/types/firebase";
-import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
-import { User } from "lucide-react";
-import useRealtimeValue from "@/utils/firebase/use-realtime-value";
+import type { ChatMessage } from "@/types/firebase";
+import { motion } from "framer-motion";
 
 const ChatContainer = ({
   message,
   friendId,
-  currentUserId,
-  beforeMessage,
 }: {
   message: ChatMessage;
   friendId: string;
-  currentUserId: string;
-  beforeMessage: ChatMessage;
 }) => {
-  const {
-    data: friend,
-    isLoading: friendLoading,
-    error: friendError,
-  } = useRealtimeValue("users/" + friendId);
-  const {
-    data: currentUser,
-    isLoading: currentUserLoading,
-    error: currentUserError,
-  } = useRealtimeValue("users/" + currentUserId);
-
-  if (friendLoading || currentUserLoading || !message) {
-    return <></>;
-  }
-
-  if (friendError || currentUserError) {
-    return <></>;
-  }
+  const time = new Date(message.timestamp);
+  const formattedTime = `${time.getHours().toString().padStart(2, "0")}:${time
+    .getMinutes()
+    .toString()
+    .padStart(2, "0")}`;
 
   return message.sender === friendId ? (
-    <div className="w-full grid place-items-start">
-      <div
-        className="md:max-w-[75%] max-w-[90%] w-full flex flex-row-reverse gap-2 items-end justify-end  "
-        style={{
-          paddingLeft: beforeMessage?.sender === message?.sender ? "48px" : "",
-        }}
-      >
-        <div className="w-fit bg-black p-2 h-fit rounded-lg min-w-10">
-          <p className="text-sm text-white">{message?.text}</p>
-        </div>
-        {beforeMessage?.sender === message?.sender ? null : (
-          <Avatar className="w-10 h-10">
-            <AvatarImage src={(friend as FirebaseUser)?.photoURL} />
-            <AvatarFallback>
-              <User />
-            </AvatarFallback>
-          </Avatar>
-        )}
-      </div>
-    </div>
+    <motion.div
+      className="w-fit bg-black/5 p-2 h-fit rounded-lg rounded-tl-none min-w-10 md:max-w-[75%] max-w-[90%] mr-auto"
+      initial={{
+        y: 10,
+        x: -5,
+        scale: 0.95,
+      }}
+      animate={{
+        y: 0,
+        x: 0,
+        scale: 1,
+      }}
+      transition={{
+        ease: "linear",
+        duration: 0.1,
+      }}
+    >
+      <p className="text-sm text-black">{message.text}</p>
+      <p className="text-[12px] text-black/50 w-full text-end">
+        {formattedTime}
+      </p>
+    </motion.div>
   ) : (
-    <div className="w-full grid place-items-end ">
-      <div
-        className="md:max-w-[75%] max-w-[90%] w-full flex justify-end gap-2 items-end"
-        style={{
-          paddingRight: beforeMessage?.sender === message?.sender ? "48px" : "",
-        }}
-      >
-        <div className="w-fit bg-black p-2 h-fit rounded-lg min-w-10">
-          <p className="text-sm text-white">{message?.text}</p>
-        </div>
-        {beforeMessage?.sender === message?.sender ? null : (
-          <Avatar className="w-10 h-10">
-            <AvatarImage src={(currentUser as FirebaseUser)?.photoURL} />
-            <AvatarFallback>
-              <User />
-            </AvatarFallback>
-          </Avatar>
-        )}
-      </div>
-    </div>
+    <motion.div
+      className="w-fit bg-black p-2 h-fit rounded-lg rounded-tr-none min-w-10 md:max-w-[75%] max-w-[90%] ml-auto"
+      initial={{
+        y: 10,
+        x: 5,
+        scale: 0.95,
+      }}
+      animate={{
+        y: 0,
+        x: 0,
+        scale: 1,
+      }}
+      transition={{
+        ease: "linear",
+        duration: 0.1,
+      }}
+    >
+      <p className="text-sm text-white">{message.text}</p>
+      <p className="text-[12px] text-white/50 w-full text-end">
+        {formattedTime}
+      </p>
+    </motion.div>
   );
 };
 
--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,19 +1,22 @@
 import ChatContainer from "@/components/ChatContainer";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { useUser } from "@/context/user";
-import type { ChatMessage } from "@/types/firebase";
+import type { ChatMessage, FirebaseUser } from "@/types/firebase";
+import { withProtected } from "@/utils/auth/use-protected";
 import getObjectValues from "@/utils/firebase/get-object-values";
+import useRealtimeValue from "@/utils/firebase/use-realtime-value";
 import {
   getOrCreateChat,
   sendMessage,
   useChatMessages,
 } from "@/utils/friends/use-chat";
 import useFriends from "@/utils/friends/use-friends";
-import { SendHorizonal } from "lucide-react";
+import { SendHorizonal, User } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
@@ -23,12 +26,17 @@ type MessageInput = {
   message: string;
 };
 
-const ChatPage = () => {
+const Chat = () => {
   const { friendId } = useParams<{ friendId: string }>();
   const { uid } = useUser();
   const [chatId, setChatId] = useState<string | null>(null);
   const { areFriends } = useFriends();
   const { register, handleSubmit, reset, watch } = useForm<MessageInput>();
+  const {
+    data: friendProfile,
+    isLoading: friendLoading,
+    error: friendError,
+  } = useRealtimeValue(`users/${friendId}`);
 
   useEffect(() => {
     const initChat = async () => {
@@ -42,12 +50,6 @@ const ChatPage = () => {
   }, [uid, friendId, areFriends]);
 
   const { data: messages, isLoading, error } = useChatMessages(chatId ?? "");
-
-  useEffect(() => {
-    if (messages) {
-      console.log(getObjectValues(messages));
-    }
-  }, [messages, isLoading, error]);
 
   const handleSend = async (data: MessageInput) => {
     if (!watch("message").trim() || !chatId) return;
@@ -64,37 +66,46 @@ const ChatPage = () => {
     ? getObjectValues(messages).sort((a, b) => a.timestamp - b.timestamp)
     : [];
 
-  if (!chatId || isLoading || !chatList) {
+  if (!chatId || isLoading || !chatList || friendLoading) {
     return <div>Loading...</div>;
   }
 
-  if (error) {
+  if (error || friendError) {
     return <div>Something error...</div>;
   }
 
   return (
     <div className="w-full max-h-svh min-h-svh flex justify-center overflow-y-scroll">
       <div className="max-w-3xl w-full py-3 px-4 md:px-0 h-full flex flex-col">
-        <div className="w-full flex-col flex gap-1">
+        <div className="w-full flex items-center gap-4 bg-white p-2">
+          <Avatar className="w-10 h-10">
+            <AvatarImage src={(friendProfile as FirebaseUser).photoURL} />
+            <AvatarFallback>
+              <User />
+            </AvatarFallback>
+          </Avatar>
+          <p className="font-bold text-sm">
+            {(friendProfile as FirebaseUser).displayName}
+          </p>
+        </div>
+        <div className="w-full flex-col flex gap-1 pb-30 max-h-[calc(100svh-100px)] overflow-y-scroll hide-scroll">
           {chatList.map((message: ChatMessage, idx) => {
             return (
               <ChatContainer
-                beforeMessage={chatList[idx - 1]}
                 key={`${message.timestamp}-${message.sender}-${idx}`}
                 message={message}
                 friendId={friendId!}
-                currentUserId={uid!}
               />
             );
           })}
         </div>
-        <div className="w-full fixed bottom-0 right-0 grid place-items-center pb-10 px-4">
+        <div className="w-full fixed bottom-0 right-0 grid place-items-center pb-10 px-4 bg-white pt-3">
           <form
             onSubmit={handleSubmit(handleSend)}
             className="w-full max-w-3xl relative"
           >
             <TextareaAutoSize
-              className="w-full text-sm px-4 py-2 bg-black/5 focus:outline-2 focus:outline-black/50 focus:rounded-sm font-jakarta focus:bg-[#DFDFDF]/25 resize-none hide-scroll"
+              className="w-full text-sm px-4 py-2 bg-black/5 focus:outline-2 focus:outline-black/50 focus:rounded-lg font-jakarta focus:bg-[#DFDFDF]/25 resize-none hide-scroll rounded-full"
               minRows={1}
               maxRows={10}
               placeholder="Type a message"
@@ -126,4 +137,5 @@ const ChatPage = () => {
   );
 };
 
+const ChatPage = withProtected(Chat);
 export default ChatPage;
